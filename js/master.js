8cd5c6aadb4cdf1dd45e72fb685e0340abd210f8
Kingsley <k@n.com>
2020-03-05 15:10:27 +0100

    Hooked up components to data

--- js/master.js
+++ js/master.js
@@ -1,10 +1,49 @@
+const data = {
+    title: "Todo list",
+    year: (new Date()).getFullYear(),
+    todo: {
+        items: [{
+            // Randomly generated UUID
+            "id": "123e4567-e89b-12d3-a456-426655440000",
+            // the text the user entered
+            "description": "Go to the shop",
+            // boolean – defaults to false
+            "isCompleted": true,
+        },{
+            // Randomly generated UUID
+            "id": "123e4567-e89b-12d3-a456-426655440000",
+            // the text the user entered
+            "description": "Test item 2",
+            // boolean – defaults to false
+            "isCompleted": false,
+        },{
+            // Randomly generated UUID
+            "id": "123e4567-e89b-12d3-a456-426655440000",
+            // the text the user entered
+            "description": "Test item 3",
+            // boolean – defaults to false
+            "isCompleted": true,
+        }]
+    }
+};
+
+// get the root element
+const rootElem = document.getElementById("root");
+// render app with our data
+rootElem.innerHTML = App(data);
+
 function TodoItem(props) {
+    // add completed class name if the task is completed
+    const className = "todoitem" 
+        + (props.isCompleted ? " todoitem--completed" : "");
+    
     return `
-        <div class="todoitem">
-            <button class="todoitem__description todoitem__completedtoggle">
-                Go to the shops
+        <div class="${className}" data-taskid="${props.id}">
+            <button class="todoitem__description todoitem__completedtoggle"
+                data-toggleid="${props.id}">
+                ${props.description}
             </button>
-            <button class="todoitem__remove">
+            <button class="todoitem__remove" data-removeid="${props.id}">
                 Remove
             </button>
         </div>
@@ -22,7 +61,13 @@ function TodoListItem(props) {
 function TodoList(props) {
     return `
         <ul class="todolist">
-            ${TodoListItem(props)}
+            ${
+                // render TodoListItem component for each item
+                props.items.map(function(todoItem) {
+                    return TodoListItem(todoItem);
+                })
+                .join("")
+            }
         </ul>
     `;
 }
@@ -42,17 +87,15 @@ function TodoAddForm(props) {
 function App(props) {
     return `
         <header class="mainheader">
-            <h1 class="pagetitle">To-do list</h1>
+            <h1 class="pagetitle">${props.title}</h1>
         </header>
         <main>
             ${TodoAddForm(props)}
 
-            ${TodoList(props)}
-        </main>
+            ${TodoList(props.todo)}
+        </main> 
         <footer class="mainfooter">
-            Noroff &copy; 2020
+            Noroff &copy; ${props.year}
         </footer>
     `;
 }
-
-document.body.innerHTML = App({});
